c8a28de8dac41546fd3766597a1681b7b7a6117d
Fendyx <ferminpioridadprofesional@gmail.com>
2026-09-06 13:37:03 -0400

    Create config.js

---
+++ js/core/config.js
@@ -0,0 +1,14 @@
+export const CONFIG = {
+  APP_NAME: 'Fendyx',
+  APP_VERSION: '0.1.0',
+  SUPABASE_URL: 'https://hzyjbwnedgpjzbaodilk.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6eWpid25lZGdwanpiYW9kaWxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MDUzNDMsImV4cCI6MjEwNDI4MTM0M30.koUX5Xi-eztsqztGlqRG0vagHFM642mcZCI_uR6lJb8',
+  JITSI_DOMAIN: 'meet.jit.si',
+  ROLES: { USER: 'user', RESTAURANT: 'restaurant', DELIVERY: 'delivery', CLUB: 'club', REMOTE_WORKER: 'remote-worker' },
+  TOKEN_VALUE_USD: 1,
+  CLUB_GEOFENCE_RADIUS: 20,
+  DEFAULT_ROUTE: 'login',
+  MIN_PASSWORD_LENGTH: 8,
+  MIN_AGE: 18
+};
+window.CONFIG = CONFIG;
